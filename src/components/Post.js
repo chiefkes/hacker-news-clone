867a429ada999116d6bff37e2f6e429baa7a706a
chiefkes <max.liefkes@gmail.com>
2021-08-17 19:27:22 +0100

    refactor User component to use React Hooks

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useReducer } from "react";
+import PropTypes from "prop-types";
 import queryString from "query-string";
 import { getItem, getComments } from "../utils/api";
 import Loading from "./Loading";
@@ -89,86 +90,6 @@ export default function Post({ location: { search } }) {
   );
 }
 
-/* 
-export default class Post extends Component {
-  state = {
-    loading: true,
-    post: null,
-    comments: null,
-    error: null,
-  };
-
-  componentDidMount() {
-    const { location } = this.props;
-    getItem(queryString.parse(location.search).id)
-      .then((post) => {
-        this.setState({
-          post,
-          error: null,
-          loading: true,
-        });
-
-        return post;
-      })
-      .then(({ kids }) => {
-        if (kids) {
-          return getComments(kids);
-        }
-        return null;
-      })
-      .then((comments) => {
-        this.setState({
-          comments,
-          error: null,
-          loading: false,
-        });
-      })
-      .catch(({ message }) =>
-        this.setState({
-          posts: null,
-          comments: null,
-          error: message,
-          loading: false,
-        })
-      );
-  }
-
-  render() {
-    const { post, comments, error } = this.state;
-
-    return (
-      <>
-        {error && <p className="center-text error">{error}</p>}
-        {post ? (
-          <>
-            <h1 className="header">
-              <a className="link" href={post.url}>
-                {post.title}
-              </a>
-            </h1>
-            <MetaInfo
-              id={post.id}
-              by={post.by}
-              time={post.time}
-              descendants={post.descendants}
-            />
-            <p
-              dangerouslySetInnerHTML={{
-                __html: post.text,
-              }}
-            />
-            {post.descendants > 0 &&
-              (comments ? (
-                <Comments comments={comments} />
-              ) : (
-                <Loading text="Fetching comments" />
-              ))}
-          </>
-        ) : (
-          <Loading text="Fetching post" />
-        )}
-      </>
-    );
-  }
-}
- */
+Post.propTypes = {
+  location: PropTypes.object.isRequired,
+};
--- src/components/User.js
+++ src/components/User.js
@@ -1,68 +1,87 @@
-import React, { Component } from "react";
+import React, { useReducer, useEffect } from "react";
+import PropTypes from "prop-types";
 import queryString from "query-string";
 import { getUser, getPosts } from "../utils/api";
 import MetaInfo from "./MetaInfo";
 import Loading from "./Loading";
 import PostList from "./PostList";
 
-export default class User extends Component {
-  state = {
+function userReducer(state, action) {
+  switch (action.type) {
+    case "fetch":
+      return {
+        user: null,
+        posts: null,
+        error: null,
+      };
+    case "user":
+      return {
+        ...state,
+        user: action.user,
+      };
+
+    case "posts":
+      return {
+        ...state,
+        posts: action.posts,
+      };
+
+    case "error":
+      return {
+        user: null,
+        posts: null,
+        error: action.message,
+      };
+
+    default:
+      throw new Error("Unknown action type in userReducer");
+  }
+}
+
+export default function User({ location: { search } }) {
+  const [{ user, posts, error }, dispatch] = useReducer(userReducer, {
     user: null,
     posts: null,
     error: null,
-  };
+  });
 
-  componentDidMount() {
-    const { id } = queryString.parse(this.props.location.search);
-
+  useEffect(() => {
+    const { id } = queryString.parse(search);
+    dispatch({ type: "fetch" });
     getUser(id)
       .then((user) => {
-        this.setState({
-          user,
-          error: null,
-        });
+        dispatch({ type: "user", user });
         return getPosts(user.submitted.slice(0, 30));
       })
-      .then((posts) => {
-        this.setState({
-          posts,
-          error: posts.length > 0 ? null : "This user hasn't posted yet",
-        });
-      })
-      .catch(({ message }) =>
-        this.setState({
-          user: null,
-          posts: null,
-          error: message,
-        })
-      );
-  }
+      .then((posts) => dispatch({ type: "posts", posts }))
+      .catch(({ message }) => dispatch({ type: "error", message }));
+  }, [search]);
 
-  render() {
-    const { user, posts, error } = this.state;
-    return (
-      <>
-        {user ? (
-          <>
-            <h1 className="header">{user.id}</h1>
-            <MetaInfo joined={user.created} karma={user.karma} />
-            <p
-              dangerouslySetInnerHTML={{
-                __html: user.about,
-              }}
-            />
-            <h2>Posts</h2>
-            {posts ? (
-              <PostList posts={posts} />
-            ) : (
-              <Loading text="Fetching posts" />
-            )}
-          </>
-        ) : (
-          <Loading text="Fetching user" />
-        )}
-        {error && <p className="center-text error">{error}</p>}
-      </>
-    );
-  }
+  if (error) return <p className="center-text error">{error}</p>;
+
+  if (!user) return <Loading text="Fetching user" />;
+
+  return (
+    <>
+      <h1 className="header">{user.id}</h1>
+      <MetaInfo joined={user.created} karma={user.karma} />
+      <p
+        dangerouslySetInnerHTML={{
+          __html: user.about,
+        }}
+      />
+      <h2>Posts</h2>
+      {!posts ? (
+        <Loading text="Fetching posts" />
+      ) : posts.length > 0 ? (
+        <PostList posts={posts} />
+      ) : (
+        <p className="center-text error">This user hasn't posted yet</p>
+      )}
+    </>
+  );
 }
+
+User.propTypes = {
+  location: PropTypes.object.isRequired,
+};
